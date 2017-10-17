564fe20237ba0537ec3541089fb1edca6f202543
kozak <v.kozachenco@gmail.com>
2017-10-17 13:42:55 +0300

    Project crated;

---
+++ src/main/java/ua/kozak/hitman/HitmanApplication.java
@@ -0,0 +1,12 @@
+package ua.kozak.hitman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitmanApplication.class, args);
+    }
+}
